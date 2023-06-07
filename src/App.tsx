fc3871bdcc726894632579cf8706a7f44df21669
Testell <tylerestelljr@gmail.com>
2023-06-07 18:51:38 -0500

    added logo background component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Logo from './Logo/Logo';
+import Logo from './components/Logo/Logo';
+import LogoBackground from './components/LogoBackground/LogoBackground';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <Logo/>
+        <LogoBackground/>
       </header>
     </div>
   );
---
+++ src/components/LogoBackground/LogoBackground.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./LogoBackground.module.css";
+
+function LogoBackground() {
+  return <div className={styles["logoBackgroundBox"]}></div>;
+}
+
+export default LogoBackground;
