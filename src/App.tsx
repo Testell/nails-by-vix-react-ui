69ad5e89b8350bac5f3100395279d47b424855e2
Testell <tylerestelljr@gmail.com>
2023-06-07 19:22:34 -0500

    installed gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Logo from './components/Logo/Logo';
-import LogoBackground from './components/LogoBackground/LogoBackground';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Logo from "./components/Logo/Logo";
+import LogoBackground from "./components/LogoBackground/LogoBackground";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Logo/>
-        <LogoBackground/>
       </header>
     </div>
   );
