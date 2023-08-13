9118b2f95656e76486c361ac005c0bd75c6217a5
Testell <108036777+Testell@users.noreply.github.com>
2023-08-12 22:46:44 -0500

    admin dashboard page

---
+++ src/pages/Admin/AdminDashBoard/AdminDashBoard.tsx
@@ -0,0 +1,12 @@
+import styles from "./AdminDashBoard.module.css";
+
+function AdminDashBoard() {
+  
+    return (
+      <main className={styles['page']}>
+          
+      </main>
+    )
+  }
+  
+  export default AdminDashBoard;
