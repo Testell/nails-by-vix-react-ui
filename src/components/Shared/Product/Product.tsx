b3447a8efaeb2ad15a6b2049eff5166ee3d154b4
Testell <108036777+Testell@users.noreply.github.com>
2023-09-19 23:48:01 -0500

    product

--- src/components/Shared/Product/Product.tsx
+++ src/components/Shared/Product/Product.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { ProductType } from "../../../contexts/ProductContext/ProductProvider"
+import { ReducerActionType, ReducerAction } from "../../../contexts/CartContext/CartContextProvider"
+import { ReactElement } from "react"
 
-const Product = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+type PropsType = {
+    product: ProductType,
+    dispatch: React.Dispatch<ReducerAction>,
+    REDUCER_ACTIONS: ReducerActionType
+}
+
+const Product = ({product, dispatch, REDUCER_ACTIONS}:
+    PropsType): ReactElement => {
+
+    const onAddToCart = () => dispatch({ type: REDUCER_ACTIONS.ADD, payload: {...product, quantity: 1}})
+
+    const content = (
+        <article>
+            <h3>{product.name}</h3>
+            <p>{new Intl.NumberFormat('en-US', {style:
+                'currency' , currency: 'USD'}).format(product.price)}
+            </p>
+            <button onClick={onAddToCart}>Add To Cart</button>
+        </article>
+    )
+    
+  return content;
+  
 }
 
 export default Product
